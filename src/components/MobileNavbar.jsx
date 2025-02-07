b10c28632f01119f09f7bb77be06f150262647c7
Sabin Nayaju <raj123nayaju@gmail.com>
2025-02-07 21:13:50 +0545

    Sec

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -12,11 +12,14 @@ import {
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { useState } from "react";
-import { useAuth, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { useAuth, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 
-function MobileNavbar() {
+function MobileNavbar () {
+    const {user}= useUser()
+   
+    
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const { isSignedIn } = useAuth();
   const { theme, setTheme } = useTheme();
@@ -61,7 +64,7 @@ function MobileNavbar() {
                   </Link>
                 </Button>
                 <Button variant="ghost" className="flex items-center gap-3 justify-start" asChild>
-                  <Link href="/profile">
+                  <Link href={`/profile/${user?.username}`}>
                     <UserIcon className="w-4 h-4" />
                     Profile
                   </Link>
